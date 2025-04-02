f3d28b32f61d12ab34eb5f95f330da1c544134e0
shencheng <32024826@qq.com>
2025-04-02 17:06:02 +0800

    feat: 🎸 初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- Start Generation Here -->
+    <div class="login-container">
+        <button class="login-button" type="primary" open-type="getUserInfo" @getuserinfo="getUserInfo">登录</button>
+    </div>
+    <!-- End Generation Here -->
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const getUserInfo = () => {
+    console.log('111');
+    
+}
+</script>
+
+<style></style>
